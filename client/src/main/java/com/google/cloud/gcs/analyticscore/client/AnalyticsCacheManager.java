bfcee0115925bc0da93ce507200187ca4637df6c
pbeerelly <prudhvimaharishi@gmail.com>
2026-06-29 16:07:32 +0530

    Making the object cache static.

--- client/src/main/java/com/google/cloud/gcs/analyticscore/client/AnalyticsCacheManager.java
+++ client/src/main/java/com/google/cloud/gcs/analyticscore/client/AnalyticsCacheManager.java
@@ -22,6 +22,7 @@ import com.github.benmanes.caffeine.cache.Weigher;
 import com.google.cloud.gcs.analyticscore.common.cache.AnalyticsCache;
 import com.google.cloud.gcs.analyticscore.common.cache.AnalyticsCacheCaffeineImpl;
 import com.google.cloud.gcs.analyticscore.common.cache.AnalyticsCacheNoOpImpl;
+import com.google.common.annotations.VisibleForTesting;
 import java.io.IOException;
 import java.nio.ByteBuffer;
 import java.util.Optional;
@@ -32,6 +33,9 @@ import java.util.concurrent.TimeUnit;
  * various specialized caches (e.g., Parquet footer cache).
  */
 public class AnalyticsCacheManager {
+
+  private static final Object LOCK = new Object();
+  private static AnalyticsCache<GcsObjectChunkKey, ByteBuffer> sharedObjectChunkCache;
 
   /**
    * TTL for bucket properties cache in minutes. A 10-minute TTL is used because a bucket's
@@ -49,6 +53,7 @@ public class AnalyticsCacheManager {
    *
    * @param options The configuration options for the caching layer.
    */
+  @SuppressWarnings("StaticAssignmentInConstructor")
   public AnalyticsCacheManager(GcsCacheOptions options) {
     checkNotNull(options, "options cannot be null");
     Weigher<GcsItemId, ByteBuffer> weigher = (key, value) -> value.remaining();
@@ -60,12 +65,20 @@ public class AnalyticsCacheManager {
         options.isSmallObjectCacheEnabled()
             ? AnalyticsCacheCaffeineImpl.create(options.getSmallObjectCacheMaxSizeBytes(), weigher)
             : AnalyticsCacheNoOpImpl.getInstance();
-    Weigher<GcsObjectChunkKey, ByteBuffer> chunkWeigher = (key, value) -> value.remaining();
-    this.objectChunkCache =
-        options.isObjectChunkCacheEnabled()
-            ? AnalyticsCacheCaffeineImpl.create(
-                options.getObjectChunkCacheMaxSizeBytes(), chunkWeigher)
-            : AnalyticsCacheNoOpImpl.getInstance();
+
+    if (options.isObjectChunkCacheEnabled()) {
+      synchronized (LOCK) {
+        if (sharedObjectChunkCache == null) {
+          Weigher<GcsObjectChunkKey, ByteBuffer> chunkWeigher = (key, value) -> value.remaining();
+          sharedObjectChunkCache =
+              AnalyticsCacheCaffeineImpl.create(
+                  options.getObjectChunkCacheMaxSizeBytes(), chunkWeigher);
+        }
+      }
+      this.objectChunkCache = sharedObjectChunkCache;
+    } else {
+      this.objectChunkCache = AnalyticsCacheNoOpImpl.getInstance();
+    }
     this.bucketPropertiesCache =
         AnalyticsCacheCaffeineImpl.createWithTtlOnly(
             BUCKET_PROPERTIES_CACHE_TTL_MINUTES, TimeUnit.MINUTES);
@@ -182,6 +195,13 @@ public class AnalyticsCacheManager {
     smallObjectCache.invalidateAll();
     objectChunkCache.invalidateAll();
     bucketPropertiesCache.invalidateAll();
+  }
+
+  @VisibleForTesting
+  static void resetSharedObjectChunkCache() {
+    synchronized (LOCK) {
+      sharedObjectChunkCache = null;
+    }
   }
 
   /** A loader for GCS object footers. */
